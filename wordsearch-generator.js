32834ac1a7a6293e6b1899438abbfa1a18246d2c
Michael Harms <michael.harms6010@gmail.com>
2020-03-21 18:42:26 -0500

    updates algo

--- wordsearch-generator.js
+++ wordsearch-generator.js
@@ -11,104 +11,215 @@ function randomFill(wordsearch){
   }
   
   function addWord(word,wordsearch){
-    let directions = ["row", "column", "diagup", "diagdown", "rowrev", "colrev", "diagrevup", "diagrevdown"];
-    let x, y, row, col;
+    let directions = ["row", "column", "diagdown", "diagup",  "rowrev", "colrev", "diagrevup", "diagrevdown"];
+    let x, y = 0
     let valid = false
     while (!valid) {
       let randIndex = Math.floor(Math.random() * 6);
-      randIndex=Math.floor(Math.random() * 2);
+      randIndex=Math.floor(Math.random() * 8);
       console.log(randIndex)
-      valid = true
+      valid = true 
+    
   
       if (directions[randIndex] === "row") {
-        x = 1;
-        y = 0;
-      } else if (directions[randIndex] === "column") {
-        x = 0;
-        y = 1;
-      } else if (directions[randIndex] === "diagup") {
-        x = 1;
-        y = -1;
-      } else if (directions[randIndex] === "diagdown") {
-        x = 1;
-        y = 1;
-      } else if (directions[randIndex] === "rowrev") {
-        x = -1;
-        y = 0;
-      } else if (directions[randIndex] === "colrev") {
-        x = 0;
-        y = -1;
-      } else if (directions[randIndex] === "diagrevup") {
-        x = -1;
-        y = -1;
-      } else if (directions[randIndex] === "diagrevdown") {
-        x = -1;
-        y = 1;
-      }
-    
-      console.log(x)
-  
-  
-        if (x != 0) {
-          row=Math.floor(Math.random() * (x*12 - x * word.length))
-        } else {
-          row=Math.floor(Math.random()  *12)
-        }
-        if (y != 0) {
-          col=Math.floor(Math.random() * (y*12 - y * word.length))
-        } else {
-          col=Math.floor(Math.random()  *12)
-        }
-  
+        let row=Math.floor(Math.random() * 12)
+        let col=Math.floor(Math.random() * (13 - word.length))
         let rewind = [];
+        
         for (let i = 0; i< word.length; i++){
-          console.log(row)
-          console.log(col)
-          if (valid === true && wordsearch[row + (i * y)][col+(i * x)] === "-" || wordsearch[row + (i * y)][col+(i * x)] === word[i]) {
-            if (wordsearch[row + (i * y)][col+(i * x)] != word[i]){
-              
-              rewind.push([row + (i * y), col+(i * x)])
+          if (valid===true && wordsearch[row][col+i] === "-" || wordsearch[row][col+i] === word[i]) {
+            if (wordsearch[row][col+i] != word[i]){
+              rewind.push([row, col+i])
             }
             wordsearch[row][col+i]=word[i]
             
           } else {
             for (let j = 0; j < rewind.length; j++) {
-              console.log(rewind)
               wordsearch[rewind[j][0]][rewind[j][1]] = "-";
             }
             rewind = []
             valid= false
           }
         }
-      
+      }
   
-      // else if (directions[randIndex] === "column") {
-      //   let row=0
-      //   //Math.floor(Math.random()  * (13 - word.length))
-      //   let col=Math.floor(Math.random() * 12)
-      //   let rewind = [];
+      else if (directions[randIndex] === "column") {
+        let row=Math.floor(Math.random() * (12 - word.length))
+        //Math.floor(Math.random()  * (13 - word.length))
+        let col=Math.floor(Math.random() * 12)
+        let rewind = [];
         
-      //   for (let i = 0; i< word.length; i++){
-      //     if (valid===true &&wordsearch[row+i][col] === "-" || wordsearch[row+i][col] === word[i] ) {
-      //       if (wordsearch[row+i][col] != word[i]){
-      //         rewind.push([row+i, col])
-      //       }
-      //       wordsearch[row+i][col]=word[i]
+        for (let i = 0; i< word.length; i++){
+          if (valid === true && wordsearch[row+i][col] === "-" || wordsearch[row+i][col] === word[i]) {
+            if (wordsearch[row+i][col] != word[i]){
+              rewind.push([row+i, col])
+            }
+            wordsearch[row+i][col]=word[i]
             
-      //     } else {
-      //       console.log(rewind)
-      //       for (let j = 0; j < rewind.length; j++) {
-      //         wordsearch[rewind[j][0]][rewind[j][1]] = "-";
-      //       }
-      //       rewind = []
-      //       valid= false
-      //     }
-      //   }
-      // }
+          } else {
+            console.log(rewind)
+            for (let j = 0; j < rewind.length; j++) {
+              wordsearch[rewind[j][0]][rewind[j][1]] = "-";
+            }
+            rewind = []
+            valid= false
+          }
+        }
+      }
   
-      // else {
+      else if (directions[randIndex] === "diagdown") {
+        console.log("attempting diag")
+        let row=Math.floor(Math.random() * (12 - word.length))
+        //Math.floor(Math.random()  * (13 - word.length))
+        let col=Math.floor(Math.random() * (12 - word.length))
+        let rewind = [];
+        
+        for (let i = 0; i< word.length; i++){
+          if (valid === true && wordsearch[row+i][col+i] === "-" || wordsearch[row+i][col+i] === word[i]) {
+            if (wordsearch[row+i][col+i] != word[i]){
+              rewind.push([row+i, col+i])
+            }
+            wordsearch[row+i][col+i]=word[i]
+            
+          } else {
+            console.log(word, "rewinding diagdown", rewind)
+            for (let j = 0; j < rewind.length; j++) {
+              wordsearch[rewind[j][0]][rewind[j][1]] = "-";
+            }
+            rewind = []
+            valid= false
+          }
+        }
+      }
   
-      // }
+      else if (directions[randIndex] === "diagup") {
+        console.log("attempting upward diag")
+        console.log(word)
+        let col=Math.floor(Math.random() * (12 - word.length))
+        //Math.floor(Math.random()  * (13 - word.length))
+        let row=word.length + Math.floor(Math.random() * (12 - word.length))
+        let rewind = [];
+        
+        for (let i = 0; i< word.length; i++){
+          if (valid===true && wordsearch[row-i][col+i] === "-" || wordsearch[row-i][col+i] === word[i]) {
+            if (wordsearch[row-i][col+i] != word[i]){
+              rewind.push([row-i, col+i])
+              console.log(rewind)
+            }
+            wordsearch[row-i][col+i]=word[i]
+            
+          } else {
+            for (let j = 0; j < rewind.length; j++) {
+              wordsearch[rewind[j][0]][rewind[j][1]] = "-";
+            }
+            rewind = []
+            valid= false
+          }
+        }
+      }
+      else if (directions[randIndex] === "rowrev") {
+        console.log("reverse row")
+        console.log(word)
+        let row=Math.floor(Math.random() * (12 - word.length))
+        //Math.floor(Math.random()  * (13 - word.length))
+        let col=word.length + Math.floor(Math.random() * (12 - word.length))
+        let rewind = [];
+        
+        for (let i = 0; i< word.length; i++){
+          if (valid===true && wordsearch[row][col-i] === "-" || wordsearch[row][col-i] === word[i]) {
+            if (wordsearch[row][col-i] != word[i]){
+              rewind.push([row, col-i])
+              console.log(rewind)
+            }
+            wordsearch[row][col-i]=word[i]
+            
+          } else {
+            for (let j = 0; j < rewind.length; j++) {
+              wordsearch[rewind[j][0]][rewind[j][1]] = "-";
+            }
+            rewind = []
+            valid= false
+          }
+        }
+      }
+  
+      else if (directions[randIndex] === "colrev") {
+        console.log("colrev")
+        console.log(word)
+        let col=Math.floor(Math.random() * 12)
+        //Math.floor(Math.random()  * (13 - word.length))
+        let row=word.length + Math.floor(Math.random() * (12 - word.length))
+        let rewind = [];
+        
+        for (let i = 0; i< word.length; i++){
+          if (valid===true && wordsearch[row-i][col] === "-" || wordsearch[row-i][col] === word[i]) {
+            if (wordsearch[row-i][col] != word[i]){
+              rewind.push([row-i, col])
+              console.log(rewind)
+            }
+            wordsearch[row-i][col]=word[i]
+            
+          } else {
+            for (let j = 0; j < rewind.length; j++) {
+              wordsearch[rewind[j][0]][rewind[j][1]] = "-";
+            }
+            rewind = []
+            valid= false
+          }
+        }
+      }
+  
+      else if (directions[randIndex] === "diagrevdown") {
+        console.log("diagrevdown")
+        console.log(word)
+        let col=word.length + Math.floor(Math.random() * (12 - word.length))
+        //Math.floor(Math.random()  * (13 - word.length))
+        let row=Math.floor(Math.random() * (12 - word.length))
+        let rewind = [];
+        
+        for (let i = 0; i< word.length; i++){
+          if (valid===true && wordsearch[row+i][col-i] === "-" || wordsearch[row+i][col-i] === word[i]) {
+            if (wordsearch[row+i][col-i] != word[i]){
+              rewind.push([row+i , col-i])
+              console.log(rewind)
+            }
+            wordsearch[row+i][col-i]=word[i]
+            
+          } else {
+            for (let j = 0; j < rewind.length; j++) {
+              wordsearch[rewind[j][0]][rewind[j][1]] = "-";
+            }
+            rewind = []
+            valid= false
+          }
+        }
+      }
+  
+      else if (directions[randIndex] === "diagrevup") {
+        console.log("diagrevup")
+        console.log(word)
+        let col=word.length + Math.floor(Math.random() * (12 - word.length))
+        //Math.floor(Math.random()  * (13 - word.length))
+        let row=word.length + Math.floor(Math.random() * (12 - word.length))
+        let rewind = [];
+        
+        for (let i = 0; i< word.length; i++){
+          if (valid===true && wordsearch[row-i][col-i] === "-" || wordsearch[row-i][col-i] === word[i]) {
+            if (wordsearch[row-i][col-i] != word[i]){
+              rewind.push([row-i,col-i])
+              console.log(rewind)
+            }
+            wordsearch[row-i][col-i]=word[i]
+            
+          } else {
+            for (let j = 0; j < rewind.length; j++) {
+              wordsearch[rewind[j][0]][rewind[j][1]] = "-";
+            }
+            rewind = []
+            valid= false
+          }
+        }
+      }
   
   
     }
@@ -144,7 +255,7 @@ function randomFill(wordsearch){
   addWord("JAVASCRIPT",wordsearch)    
   addWord("ALGORITHM",wordsearch)    
   addWord("WORDSEARCH",wordsearch)    
-  addWord("MIKEY",wordsearch)
+  addWord("KRISTI",wordsearch)
   
   
   randomFill(wordsearch)
